9950332868f58a803509311a58dfa0d84eebd35a
beans-94 <62425506+beans-94@users.noreply.github.com>
2023-02-20 13:44:40 +0530

    Update UlamSpiral.py

--- UlamSpiral.py
+++ UlamSpiral.py
@@ -10,11 +10,11 @@ def isprime(n):
     
 
 
-def ulam_spiral():
+def ulam_spiral(numOfSteps):
     t = turtle.Turtle()
     step = 1
     stepSize = 20
-    numSteps = 1
+    stepCount = 1
     turnCounter = 0
     t.width(2)
     for i in range(200):
@@ -23,33 +23,13 @@ def ulam_spiral():
             t.dot()
             print(step)
         t.speed(1000)
-        if (step % numSteps == 0):
+        if (step % stepCount == 0):
             t.left(90)
             turnCounter += 1
             if (turnCounter % 2 == 0):
-                numSteps += 1
-        step += 1
-    turtle.done()
-
-
-def ulamspiral(numOfSteps):
-    step = 1
-    stepCount = 1
-    stepSize = 20
-    turnCounter = 1
-    t = turtle.Turtle()
-    for i in range(numOfSteps):
-        t.width(3)
-        t.forward(stepSize)
-        if isprime(step):
-            t.dot()
-        if  step % stepCount == 0:
-            t.left(90)
-            turnCounter += 1
-            if turnCounter % 2 == 0:
                 stepCount += 1
         step += 1
     turtle.done()
 
-print(ulamspiral(200))
+print(ulam_spiral(200))
 
